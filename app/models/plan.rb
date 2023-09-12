c95b93788ea0be3c21ca48d608e67d6285862250
himi513 <hokada513@gmail.com>
2023-09-12 17:50:09 +0900

    再修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,15 +1,4 @@
 class Plan < ApplicationRecord
   validates :plan, presence: true
-  validates :date, presence: true
-
-  def create
-    @plan = Plan.create(plan_params)
-  
-    if @plan.save
-      redirect_to @plan
-    else
-      render 'new'
-    end
-  end
-  
+  validates :date, presence: true  
 end
